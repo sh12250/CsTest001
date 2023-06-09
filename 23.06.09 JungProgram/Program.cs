9044911ed81726b4b83ced589b05dc53a5711e9a
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-09 17:48:53 +0900

    Rerotate. C# 프로젝트 위치 옮김

---
+++ 23.06.09 JungProgram/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// 일정한 주기로 코인이 랜덤한 위치에 떨어진다
+// 플레이어 ( 0 )가 코인을 주으면 점수가 상승
+
+// 플레이어와 컴퓨터가 각각 카드를 2장씩 뽑는다
+// 두 카드의 합이 더 큰 쪽이 승리, 작은 쪽은 패배한다
+// 동일한 숫자가 나올 경우 문양은
+// 스페이드, 다이아몬드, 하트, 클로버 순으로 승리한다
+
+namespace _23._06._09_JungProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] cards = new int[4];
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i] = random.Next(1, 52);
+            }
+
+            int myCard1 = cards[0];
+            string myPatt1 = ReturnPattern(cards[0] / 13);
+
+            int myCard2 = cards[1];
+            string myPatt2 = ReturnPattern(cards[1] / 13);
+
+            int comCard1 = cards[2];
+            string comPatt1 = ReturnPattern(cards[2] / 13);
+
+            int comCard2 = cards[3];
+            string comPatt2 = ReturnPattern(cards[3] / 13);
+
+            if(myCard1 + myCard2 > comCard1 + comCard2)
+            {
+                Console.WriteLine("승리");
+            }
+            else if(myCard1 + myCard2 == comCard1 + comCard2)
+            {
+                if (myCard1 > myCard2)
+                {
+                    if(comCard1 > comCard2)
+                    {
+                        if(myCard1 > comCard1)
+                        {
+
+                        }
+                    }
+                    else if(comCard2 > comCard1 && myCard1 > comCard2)
+                    {
+
+                    }
+                }
+                else if (myCard1 == myCard2)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("패배");
+            }
+
+
+
+
+
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                Console.WriteLine(cards[i] % 13 + 1);
+            }
+
+
+        }           // main()
+
+
+        static string ReturnPattern(int num)
+        {
+            switch(num)
+            {
+                case 0:
+                    return "♣";
+                case 1:
+                    return "♡";
+                case 2:
+                    return "◇";
+                case 3:
+                    return "♠";
+                default:
+                    return "0";
+            }
+        }
+        static void Shuffle(int[] numArr)
+        {
+
+
+
+
+        }
+    }           // class program()
+}
---
+++ Gustation/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// C#에서 함수는 메서드다
+// 클래스 안의 함수를 멤버 함수라고 한다
+// 그게 메서드다
+
+// 백버퍼
+
+namespace Gustation
+{
+    public class Program    // internal
+    {
+        // main함수는 변수를 밖( 명령 프롬프트 같은 것 )에서 매개변수로 받을 수 있다
+        static void Main(string[] args) // static 메서드는 함수 안에서만 부를 수 있다
+        {
+            Random random = new Random();
+            int[] lottos = new int[6];
+
+            for(int i=0;i<6; i++) 
+            {
+                lottos[i] = random.Next(1, 45);
+            }
+
+            foreach(int lotto_ in lottos )
+            {
+                Console.Write("{0} ", lotto_);
+                Thread.Sleep(1000);
+                // Task.Delay(1000).Wait();
+            }
+
+            Console.WriteLine();
+
+            
+        }           // main()
+
+
+    }       // class program
+
+    public class A
+    {
+
+        public void PrintMyArr(int[,] array_)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    // 줄넘김이 없다
+                    Console.Write("{0} ", array_[y, x]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }       // PrintMyArr();
+
+        public void Descript1()
+        {
+            // 기본적으로 0으로 초기화 되어있다
+            int[] numbers1 = new int[5];
+            int[,] numbers2 = new int[5, 5];
+
+            int valueCount = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    valueCount++;
+                    numbers2[i, j] = valueCount;
+                    // Console.WriteLine("numbers[{0}][{2}] 의 값 : {1} \n", i, numbers2[i,j], j);
+                }
+                // Console.WriteLine("numbers[{0}] 의 값 : {1} \n", i, numbers1[i]);
+            }
+            A myClassA = new A();
+
+            myClassA.PrintMyArr(numbers2);
+
+            string[,] board = new string[5, 5];
+
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    board[y, x] = "*";
+                }
+            }
+
+            Console.WriteLine("Hello world! \n");
+
+            string userInput1 = default;
+            string userInput2 = default;
+            int number = default;
+            float floatNum = default;
+
+            int userNumber1 = default;
+            int userNumber2 = default;
+
+            // 여기서 입력받는다
+            userInput1 = Console.ReadLine(); // _getch()함수 같이 작동한다
+            userInput2 = Console.ReadLine();
+
+            //userNumber1 = System.Convert.ToInt32(userInput1);
+            //userNumber2 = System.Convert.ToInt32(userInput2);
+
+            //userNumber1 = int.Parse(userInput1);
+            //userNumber2 = int.Parse(userInput2);
+
+            int.TryParse(userInput1, out userNumber1); // 숫자면 리턴?
+            int.TryParse(userInput2, out userNumber2); // 아니면 안한다?
+
+
+            Console.WriteLine("{0} + {1} = {2} \n", userNumber1, userNumber2, userNumber1 + userNumber2);
+
+
+            // Console.WriteLine("입력 받은 내용 출력 >> {1} {0} \n\n", userInput1, userInput2);
+            // {0}은 자리 표시자
+            // 배열에서의 순서와 같다
+        }
+
+    }
+
+
+
+}
