ecb106d46699c9ffc0fdf8aeca8d184eb37f70ce
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-14 22:13:24 +0900

    Create. 기술 실험용 프로그램 생성, 커서 배치, 인수받아서 커서 이동, 입력값받아서 커서 이동 구현

---
+++ CsharpTest/Cursor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTest
+{
+    public class Cursor
+    {
+        private int _xPos;
+        private int _yPos;
+
+        public void Init(int xPos, int yPos)
+        {
+            _xPos = xPos;
+            _yPos = yPos;
+        }
+
+        public void Input()
+        {
+            MoveCursor(50, 20);
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            switch (keyInfo.KeyChar)
+            {
+                case 'w':
+                    if(_yPos != 0)
+                    {
+                        _yPos--;
+                    }
+                    break;
+                case 'a':
+                    if (_xPos != 0)
+                    {
+                        _xPos--;
+                    }
+                    break;
+                case 's':
+                    _yPos++;
+                    break;
+                case 'd':
+                    _xPos++;
+                    break;
+            }
+        }
+
+        public void MoveCursor()
+        {
+            Console.SetCursorPosition(_xPos, _yPos);
+        }
+        public void MoveCursor(int xPos, int yPos)
+        {
+            Console.SetCursorPosition(xPos, yPos);
+        }
+    }
+}
---
+++ CsharpTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cursor cursor = new Cursor();
+
+            Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+            cursor.Init(12, 2);
+
+            while (true)
+            {
+                cursor.Input();
+                cursor.MoveCursor();
+                Console.WriteLine("a");
+            }
+        }
+    }
+}
