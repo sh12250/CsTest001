eca065c14a609e4a12571bc9139912d65c7bac14
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-20 18:02:39 +0900

    Create. 포탈맵 생성

---
+++ Portal/Map.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal
+{
+    public class Map
+    {
+        public string[,] theMap { get; private set; }
+        public int mapSize { get; private set; }
+
+        public Map()
+        {
+            InitSize();
+
+            string[,] map = new string[mapSize, mapSize];
+
+            for (int y = 0; y < mapSize; y++)
+            {
+                for (int x = 0; x < mapSize; x++)
+                {
+                    if (y == 0 || x == 0 || y == mapSize - 1 || x == mapSize - 1)
+                    {
+                        map[y, x] = "■";
+                    }
+                }
+            }
+
+            for (int y = 1; y < mapSize - 1; y++)
+            {
+                for (int x = 1; x < mapSize - 1; x++)
+                {
+                    map[y, x] = "　";
+                }
+            }
+
+            theMap = map;
+        }
+
+        public void InitSize()
+        {
+            Random random = new Random();
+
+            mapSize = random.Next(9, 15) + 2;
+        }
+
+        public void PrintMap()
+        {
+            for (int y = 0; y < mapSize; y++)
+            {
+                for (int x = 0; x < mapSize; x++)
+                {
+                    if (theMap[y, x] == "◎")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(theMap[y, x]);
+                    }
+                    else if (theMap[y, x] == "▥")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(theMap[y, x]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(theMap[y, x]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Portal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cursor cursor = new Cursor();
+            List<Map> maps = new List<Map>();
+
+            string userInput = "0";
+
+            int totalMap = 0;
+
+            userInput = Console.ReadLine();
+            int.TryParse(userInput, out totalMap);
+
+            for(int i = 0; i < totalMap; i++)
+            {
+                maps.Add(new Map());
+            }
+
+            cursor.MoveCursor(0, 0);
+            maps[0].PrintMap();
+        }
+    }
+}
